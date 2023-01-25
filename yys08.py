813913d6a1d8fe14ea3d38ef23ef9d352c379dcc
oracleyu01 <oracleyu23@gmail.com>
2023-01-25 18:08:28 +0900

    Update yys08.py

--- yys08.py
+++ yys08.py
@@ -56,3 +56,53 @@ Streamlit. We're generating a bunch of random numbers in a loop for around
 )
 
 plotting_demo()      
+
+def  plotting_demo2():
+    money = pd.read_csv("money_data7.csv")
+    option = st.selectbox(
+        'How would you like to choice year ?',
+        ('2020', '2021', '2022'))
+
+    option2 = int(option)
+
+    st.write('You selected:', option)
+
+    money = money[:] [money['A_YEAR']== option2]
+
+    fig, ax = plt.subplots(2,2, figsize=(12,8))
+
+    plt.subplot(221)
+    plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('America rate')
+
+
+    plt.subplot(222)
+    plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('Korea rate')
+
+    plt.subplot(223)
+    plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('Kospi Rate')
+
+    plt.subplot(224)
+    plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('House Price')
+
+    st.pyplot(fig)
+    st.dataframe(money)
+
+      
+#st.set_page_config(page_title="Plotting Demo2", page_icon="📈")
+st.markdown("# Plotting Demo2")
+st.sidebar.header("Plotting Demo2")
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)
+
+plotting_demo2()      
