155d255e5982cd3479f93eaf8ec0d896105f3eb3
oracleyu01 <oracleyu23@gmail.com>
2023-01-25 17:56:50 +0900

    Update yys08.py

--- yys08.py
+++ yys08.py
@@ -10,38 +10,38 @@ money = pd.read_csv("money_data7.csv")
 with st.form(key ='Form1'):
     with st.sidebar:
    
-        option = st.selectbox(
-            'How would you like to choice year ?',
-            ('2020', '2021', '2022'))
+option = st.selectbox(
+    'How would you like to choice year ?',
+    ('2020', '2021', '2022'))
 
-        option2 = int(option)
+option2 = int(option)
 
-        st.write('You selected:', option)
+st.write('You selected:', option)
 
-        money = money[:] [money['A_YEAR']== option2]
+money = money[:] [money['A_YEAR']== option2]
 
-        fig, ax = plt.subplots(2,2, figsize=(12,8))
+fig, ax = plt.subplots(2,2, figsize=(12,8))
 
-        plt.subplot(221)
-        plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
-        plt.xticks(tuple(money['A_MONTH']) )
-        plt.title('America rate')
+plt.subplot(221)
+plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('America rate')
 
 
-        plt.subplot(222)
-        plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
-        plt.xticks(tuple(money['A_MONTH']) )
-        plt.title('Korea rate')
+plt.subplot(222)
+plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('Korea rate')
 
-        plt.subplot(223)
-        plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
-        plt.xticks(tuple(money['A_MONTH']) )
-        plt.title('Kospi Rate')
+plt.subplot(223)
+plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('Kospi Rate')
 
-        plt.subplot(224)
-        plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
-        plt.xticks(tuple(money['A_MONTH']) )
-        plt.title('House Price')
+plt.subplot(224)
+plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('House Price')
 
-        st.pyplot(fig)
-        st.dataframe(money)
+st.pyplot(fig)
+st.dataframe(money)
