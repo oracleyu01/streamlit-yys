c62ed947d1a9696ae264f0d3b3afadeb49fe6d99
oracleyu01 <oracleyu23@gmail.com>
2023-01-25 18:14:20 +0900

    Update yys08.py

--- yys08.py
+++ yys08.py
@@ -45,14 +45,16 @@ def  plotting_demo():
     st.pyplot(fig)
     st.dataframe(money)
 
-      
-#st.set_page_config(page_title="Plotting Demo", page_icon="📈")
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
+with st.form(key ='Form1'):
+    with st.sidebar:          
+        #st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+        st.markdown("# Plotting Demo")
+        st.sidebar.header("Plotting Demo")
+        st.write(
+            """This demo illustrates a combination of plotting and animation with
+        Streamlit. We're generating a bunch of random numbers in a loop for around
+        5 seconds. Enjoy!"""
+        )
 
-plotting_demo()      
+
+        plotting_demo()      
