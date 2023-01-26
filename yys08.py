3231aaa10238f5568adec8710e1cbeec7b705581
oracleyu01 <oracleyu23@gmail.com>
2023-01-26 16:54:03 +0900

    Update yys08.py

--- yys08.py
+++ yys08.py
@@ -47,12 +47,9 @@ def  plotting_demo():
 
 with st.form(key ='Form1'):
     with st.sidebar:
-        #user_word = st.sidebar.text_input("Enter a keyword", "habs")    
-        select_language = st.sidebar.radio('What do you want graph?', ('line graph', 'bar graph', 'pie graph', 'histogram graph', 'corr graph', 'word cloud', 'box graph'))
-        #include_retweets = st.sidebar.checkbox('Include retweets in data')
-        #num_of_tweets = st.sidebar.number_input('Maximum number of tweets', 100)
-        #submitted1 = st.form_submit_button(label = 'Search Twitter 🔎')
-         
         
-if select_language =='line graph':        
+        select_language = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie'))
+        
+        
+if select_language =='line':        
     plotting_demo()      
