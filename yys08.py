9e859a1c18630251a13ed3fd343fe66e7c80f892
oracleyu01 <oracleyu23@gmail.com>
2023-02-18 12:10:04 +0900

    Update yys08.py

--- yys08.py
+++ yys08.py
@@ -71,13 +71,13 @@ with st.form(key ='Form1'):
 def  emotion():
 
     uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file: 
+    if uploaded_file is not None:
         
            #1. 텍스트 파일 3개를 불러옵니다.
            origin_text = open(uploaded_file.name, encoding="utf8")
            positive     = open("pos_pol_word.txt", encoding="utf8")
            negative    = open("neg_pol_word.txt", encoding="utf8" )
-           uploaded_file.clear()
+           #uploaded_file.clear()
 
            #2. 위의 텍스트 파일 3개를 엔터로 구분해서 변수에 담습니다.
            origin = origin_text.read()    # origin_text 를 문자형 변수 origin 에 담는다
