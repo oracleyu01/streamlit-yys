a3ccb0cc9a33ba9c796e097985f53d922a603ede
oracleyu01 <oracleyu23@gmail.com>
2023-02-18 11:03:22 +0900

    Update yys08.py

--- yys08.py
+++ yys08.py
@@ -214,17 +214,23 @@ elif select_language=='긍정 부정 분석':
     with tab1:
         tab1.subheader("긍정 부정 감성 분석")
         try:
-            p_df,n_df, all_df = emotion()    
+            p_df,n_df,all_df = emotion()    
         except:
             pass
               
     with tab2:
         tab2.subheader("긍정 단어 워드 클라우드")
-        pos_word_chart(p_df)
+        try:      
+            pos_word_chart(p_df)
+        except:
+            pass
        
     with tab3:
         tab3.subheader("부정 단어 워드 클라우드")
-        nag_word_chart(n_df)
+        try:
+            nag_word_chart(n_df)
+        except:
+            pass
    
     with tab4:
         tab4.subheader("긍정단어와 부정단어 건수와 순위")
